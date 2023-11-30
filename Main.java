5cd96fa2487490b77d1fed303f132559fcd712ed
bartekSmo <97672823+bartekSmo@users.noreply.github.com>
2023-11-30 09:27:21 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,8 +8,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Kalendarz kalendarz = new Kalendarz(7);//wybor ile dni planujemy
         System.out.println("kalendarz");
-        int dzien = ktoryDzien(scanner);//wybor dnia
-        ArrayList<Spotkanie> dziennik = kalendarz.getSpotkanie(dzien);//dziennik = arraylista majaca arraylisty z danego dnia
+
+        ArrayList<Spotkanie> dziennik = kalendarz.getSpotkanie(1);//dziennik = arraylista majaca obiekty Spotkania z danego dnia
         do{
             System.out.println("1)pokazanie\n2)stworzenie\n3)usuniecie\n4)priorytet");
             int wybor = scanner.nextInt();
@@ -17,7 +17,7 @@ public class Main {
                 System.out.println("koniec programu");
                 break;
             }
-            menu(scanner,wybor,dzien,dziennik,kalendarz);
+            menu(scanner,wybor,dziennik,kalendarz);
         }while (true);
     }
     public static void show(Kalendarz kalendarz, Scanner scanner){
@@ -26,16 +26,18 @@ public class Main {
             System.out.println(wartosci.naString());
         }
     }
-    public static void menu(Scanner scanner,int wybor,int dzien, ArrayList<Spotkanie> dziennik, Kalendarz kalendarz){
+    public static void menu(Scanner scanner,int wybor, ArrayList<Spotkanie> dziennik, Kalendarz kalendarz){
         switch (wybor){
             case 1 -> show(kalendarz,scanner);
             case 2 -> {
-                dzien = ktoryDzien(scanner);// potrzebujemy wiedziec do jakiego dnia dodac
+                int dzien = ktoryDzien(scanner);// potrzebujemy wiedziec do jakiego dnia dodac
                 Spotkanie spotkanie2 = stworzSpotkanie(scanner);//tworzymy obiekt Spotkanie z danymi od usera
                 kalendarz.addSpotkanie(dzien,spotkanie2);//dodajemy obiekt do arraylisty
             }
             case 3->{
+                int dzien = ktoryDzien(scanner);
                 int ktore = ktoreSpotkanie(scanner,dziennik);//musimy wiedziec ktory index z arraylisty z dnia [[spotkanie1,spotkanie2...],[spotkanie1...]]
+                if(ktore==-1){System.out.println("nie ma takiego spotkania");break;}
                 kalendarz.removeSpotkanie(dzien, dziennik.get(ktore));//usuwamy znajac dzien i index z listy dnia
             }
             case 4 -> ktoryPriorytet(scanner, kalendarz);
@@ -45,11 +47,11 @@ public class Main {
         String opis;
         System.out.println("Podaj opis spotkania: ");
         opis = scanner.next();
-        System.out.println("Podaj czas zaczecia: ");
+        System.out.println("Podaj czas zaczecia pozniejszy niz 08:00: ");
         LocalTime czas3 = LocalTime.parse(scanner.next());
         System.out.println("Podaj czas zakonczenia: ");
         LocalTime czas4 = LocalTime.parse(scanner.next());
-        System.out.println("Podaj priorytet");
+        System.out.println("Podaj priorytet High,Medium,Low: ");
         String priorytet = scanner.next();
         Spotkanie stworzoneSpotkanie = new Spotkanie(opis,czas3,czas4,priorytet);
         if(stworzoneSpotkanie.czyWczesnie()){
