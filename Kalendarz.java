d08da7a3a9d9103a5440e55734199c3c7412c303
bartekSmo <97672823+bartekSmo@users.noreply.github.com>
2023-12-06 10:03:29 +0100

    Update Kalendarz.java

--- Kalendarz.java
+++ Kalendarz.java
@@ -3,7 +3,22 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class Kalendarz {
-    ArrayList<ArrayList<Spotkanie>> dziennik = new ArrayList<>();//arraylista arraylist[[spotaknie1,spotkanie2...],[spotkanie1,spotkanie2...],...]
+    private ArrayList<ArrayList<Spotkanie>> dziennik = new ArrayList<>();//arraylista arraylist[[spotaknie1,spotkanie2...],[spotkanie1,spotkanie2...],...]
+
+    public void dodaniePrzykladowychSpotkan(){
+        Spotkanie spotkaniePoniedzialekProba1 = new Spotkanie("sniadanie",LocalTime.of(9,30),LocalTime.of(10, 0),"High");
+        Spotkanie spotkaniePoniedzialekProba2 = new Spotkanie("obiad",LocalTime.of(12,30),LocalTime.of(13,0),"High");
+        Spotkanie spotkaniePoniedzialekProba5 = new Spotkanie("deser",LocalTime.of(13,30),LocalTime.of(14,45),"Medium");
+        Spotkanie spotkaniePoniedzialekProba4 = new Spotkanie("trening",LocalTime.of(15,30),LocalTime.of(17,0),"Low");
+        Spotkanie spotkaniePoniedzialekProba6 = new Spotkanie("kino",LocalTime.of(20,30),LocalTime.of(21,0),"Medium");
+        Spotkanie spotkaniePoniedzialekProba3 = new Spotkanie("kolacja",LocalTime.of(21,30),LocalTime.of(21,45),"Low");
+        dziennik.get(0).add(spotkaniePoniedzialekProba1);
+        dziennik.get(0).add(spotkaniePoniedzialekProba2);
+        dziennik.get(0).add(spotkaniePoniedzialekProba5);
+        dziennik.get(0).add(spotkaniePoniedzialekProba4);
+        dziennik.get(0).add(spotkaniePoniedzialekProba6);
+        dziennik.get(0).add(spotkaniePoniedzialekProba3);
+    }
     public void stworzSpotkanie(String opis, LocalTime czasPoczatku, LocalTime czasKonca, String priotytet,int dzien){
         if(czasPoczatku.isBefore(Spotkanie.NAJWCZESNIEJ)){return;}
         Spotkanie spotkanie = new Spotkanie(opis,czasPoczatku,czasKonca,priotytet);
@@ -20,21 +35,13 @@ public class Kalendarz {
     public ArrayList<Spotkanie> getSpotkanie(int dzien){
         return dziennik.get(dzien);//liste na indexie dzien [[dzien],[dzien2]]
     }
-    public ArrayList<Spotkanie>cleanSpotkania(ArrayList<Spotkanie> spotkanieWDniu, String priorytetUser){
-        ArrayList<Spotkanie>cleanedList=new ArrayList<>();
-        for(Spotkanie spotkania : spotkanieWDniu){
-            if(spotkania.getPriorytet().equalsIgnoreCase(priorytetUser)){
-                cleanedList.add(spotkania);
-            }
-        }
-        return cleanedList;
+
+    public boolean czyJestTakieSpotkanie(int dzien,int ktore){
+        return getSpotkanie(dzien).size() > ktore;
     }
-    public boolean czyJestTakieSpotkanie(int dzien,Kalendarz kalendarz,int ktore){
-        return kalendarz.getSpotkanie(dzien).size() > ktore;
-    }
-    public ArrayList<Spotkanie> cleaner(Predicate<Spotkanie>check,Kalendarz kalendarz,  int wDniu){
+    public ArrayList<Spotkanie> filtrListBy(Predicate<Spotkanie>check,  int wDniu){
         ArrayList<Spotkanie> cleanedArr = new ArrayList<>();
-        for(Spotkanie spotkanie : kalendarz.getSpotkanie(wDniu)){
+        for(Spotkanie spotkanie : getSpotkanie(wDniu)){
             if(check.test(spotkanie)){
                 cleanedArr.add(spotkanie);
             }
